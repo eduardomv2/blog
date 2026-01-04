98e9d30c04c3b51890396dde913b9380998730e0
eduardomv2 <eduardoalejandro.v.mtz@gmail.com>
2026-01-04 12:35:06 -0600

    Carpetas de Publicaciones

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,4 @@
 
-import __ASTRO_IMAGE_IMPORT_Z1KnxQv from "../../../assets/images/GatoDomestico.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2F_2026%2FPrimerPost.md";
-export default new Map([["../../../assets/images/GatoDomestico.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2F_2026%2FPrimerPost.md", __ASTRO_IMAGE_IMPORT_Z1KnxQv]]);
+import __ASTRO_IMAGE_IMPORT_ZiY6vE from "../../../../../assets/images/GatoDomestico.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2F_2026%2F_Enero%2FTI%2FPrimerPost.md";
+export default new Map([["../../../../../assets/images/GatoDomestico.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2F_2026%2F_Enero%2FTI%2FPrimerPost.md", __ASTRO_IMAGE_IMPORT_ZiY6vE]]);
 		
